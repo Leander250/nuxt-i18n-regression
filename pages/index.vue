adfa04151988e4df6ca00df4384a4b26e557677b
leander.nachtmann <leander.nachtmann@interone.de>
2024-06-04 15:14:46 +0200

    add bescheidform

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 		<h1>Nuxt I18n Starter - {{ $t("hello") }}!</h1>
 		<h1>Options!</h1>
     <div>
-      <p>{{$t("options")}}</p>
+      <p>{{$t("bescheidform.options")}}</p>
     </div>
 	</div>
 </template>
