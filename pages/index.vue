1d98ec5679806d49d11a1623a7026d54f96479df
leander.nachtmann <leander.nachtmann@interone.de>
2024-06-04 15:08:06 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+	<div>
+		<h1>Nuxt I18n Starter - {{ $t("hello") }}!</h1>
+		<h1>Options!</h1>
+    <div>
+      <p>{{$t("options")}}</p>
+    </div>
+	</div>
+</template>
